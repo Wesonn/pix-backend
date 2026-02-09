9fae85a3a498736d2188a95ceb6b914f59c388cf
Wesonn <contatoweson@gmail.com>
2026-02-08 21:38:26 -0300

    server.js

--- server.js
+++ server.js
@@ -1,34 +1,15 @@
-import express from "express";
-import fetch from "node-fetch";
+const express = require("express");
+const cors = require("cors");
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
-const ACCESS_TOKEN = process.env.MP_TOKEN;
-
-app.post("/pix", async (req, res) => {
-  const { valor } = req.body;
-
-  const r = await fetch("https://api.mercadopago.com/v1/payments", {
-    method: "POST",
-    headers: {
-      "Authorization": `Bearer ${ACCESS_TOKEN}`,
-      "Content-Type": "application/json"
-    },
-    body: JSON.stringify({
-      transaction_amount: valor,
-      description: "Deposito jogo",
-      payment_method_id: "pix",
-      payer: { email: "test_user@test.com" }
-    })
-  });
-
-  const j = await r.json();
-
-  res.json({
-    id: j.id,
-    copia: j.point_of_interaction.transaction_data.qr_code
-  });
+app.get("/", (req, res) => {
+  res.send("PIX Backend online 🚀");
 });
 
-app.listen(process.env.PORT || 3000);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log("Servidor rodando na porta " + PORT);
+});
